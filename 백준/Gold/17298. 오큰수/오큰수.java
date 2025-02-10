0808d25aafe85f8eed038a039f1dc03b7f58f683
Junyeong Bae <101334646+JunYBae@users.noreply.github.com>
2025-02-10 16:36:21 +0900

    [Gold IV] Title: 오큰수, Time: 984 ms, Memory: 335016 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -4,64 +4,49 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Queue;
+import java.util.Scanner;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
 	
-	static Stack<Integer> list;
-	static Stack<Integer> big_number;
-	static List<Integer> answer;
+	static int array[], answer[];
+	static Stack<Integer> stack = new Stack<>();
 	
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int N = Integer.parseInt(br.readLine()); // 숫자 개수
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		list = new Stack<>(); // 값 저장할 arraylist
-		big_number = new Stack<>(); // 숫자가 큰 순서대로 담을 arraylist
-		answer = new ArrayList<>(N); // 답을 저장할 arraylist
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		array = new int[N];
+		answer = new int[N];
 		
 		for (int i = 0; i < N; i++) 
-			list.add(Integer.parseInt(st.nextToken())); // 값 저장 (list)
+			array[i] = Integer.parseInt(st.nextToken());
 		
-		answer.add(-1); // 어차피 맨 뒤값은 -1 임
-		big_number.add(list.pop()); // 맨뒤값 넣고 시작
 		
-		// 오큰수 계산
-		while (!list.isEmpty())
-		{
-			int cur_number = list.pop(); // 값 하나 가져오기 뒤에서 부터
+		for (int index = N-1; index >= 0; index--) {
 			
-			while(!big_number.isEmpty())
-			{			
-				if (big_number.peek() > cur_number) { // 픽 했을때 더 클때
-					answer.add(big_number.peek()); // 픽만 해서 빼기 (또 쓸수있음!)
-					break;
-				}
-				
-				big_number.pop(); // 이전의 값 계속 제거 / 왜? 이전값보다 현재값이 더 큼 / 그게 오큰수가 됨!
-									// 이전 값은 필요 없어지기 때문!
+			int cur_num = array[index]; // 현재 번호
+			
+			// 비어있지 않으면서, 현재 번호가 더 크면
+			while(!stack.isEmpty() && !(cur_num < stack.peek()) ) {
+				stack.pop();
 			}
 			
-			if (big_number.isEmpty()) // 오큰수를 만족하는 숫자가 없을 때 
-				answer.add(-1);	// -1 넣기
-	
-			big_number.add(cur_number);	
-		
+			if (stack.isEmpty())
+				answer[index] = -1;
+			else
+				answer[index] = stack.peek();
+			
+			stack.add(cur_num);
 		}
 		
-		for (int index = answer.size() - 1; index >= 0; index--)
-			bw.write(answer.get(index) + " ");
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < answer.length; i++)
+			sb.append(answer[i] + " ");
 		
-		bw.flush();
-		
+		System.out.println(sb.toString());
 	}
-	
 }
