ed0ae4c6d23a70774c2ba3440af07de36d0df63b
Junyeong Bae <101334646+JunYBae@users.noreply.github.com>
2025-02-19 21:50:35 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1592 ms, Memory: 369132 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Point {
+	int x, y, break_wall;
+	Point(int x, int y, int break_wall) {
+		this.x = x;
+		this.y = y;
+		this.break_wall = break_wall;
+	}
+}
+
+public class Main {
+	
+	static int N, M, K, map[][];
+	static int cost[][][];
+	static int dir[][] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		cost = new int[N][M][K+1];
+		
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < M; j++) {
+				map[i][j] = str.charAt(j) - '0';
+				Arrays.fill(cost[i][j], Integer.MAX_VALUE);
+			}
+		}
+		
+		cost[0][0][0] = 1;
+		int answer = simulation(new Point(0, 0, 0));
+		System.out.println(answer);
+
+	}
+	
+	public static int simulation(Point start_p) {
+		
+		ArrayDeque<Point> queue = new ArrayDeque<>();
+		queue.add(start_p);
+		
+		while(!queue.isEmpty()) {
+			
+			int size = queue.size();			
+			for (int i = 0; i < size; i++) {
+				
+				Point point = queue.poll();
+				
+				if (point.x == N-1 && point.y == M-1)
+					return cost[point.x][point.y][point.break_wall]; 
+				
+				for (int index = 0; index < 4; index++) {
+					
+					int x = point.x + dir[index][0];
+					int y = point.y + dir[index][1];
+					
+					if (is_In(x, y)) {
+						
+						if (map[x][y] == 1 && point.break_wall < K) {
+							if (cost[point.x][point.y][point.break_wall] + 1 < cost[x][y][point.break_wall+1]) {								
+								cost[x][y][point.break_wall+1] = cost[point.x][point.y][point.break_wall] + 1;
+								queue.add(new Point(x, y, point.break_wall+1));
+							}							
+						} 
+						
+						else if (map[x][y] == 0 && cost[point.x][point.y][point.break_wall] + 1 < cost[x][y][point.break_wall]) {
+							cost[x][y][point.break_wall] = cost[point.x][point.y][point.break_wall] + 1;
+							queue.add(new Point(x, y, point.break_wall));							
+						}					
+						
+					} 									
+				}			
+			}			
+		}
+		
+		return -1;
+	}
+	
+	public static boolean is_In(int x, int y) {
+		if (x >= 0 && y >= 0 && x < N && y < M)
+			return true;
+		return false;
+	}
+}
+
