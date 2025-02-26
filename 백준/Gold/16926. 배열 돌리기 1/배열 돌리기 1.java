0a04eb6361ef73a3772ff32f065c52b168fcc47f
Junyeong Bae <101334646+JunYBae@users.noreply.github.com>
2025-02-26 14:28:21 +0900

    [Gold V] Title: 배열 돌리기 1, Time: 392 ms, Memory: 36320 KB -BaekjoonHub

--- 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -29,22 +28,26 @@ public class Main {
 			list.add(new ArrayDeque<Integer>());
 		}
 		
-		
+		// 배열에 값 저장 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " "); 
 			for (int j = 0; j < M; j++)
 				arr[i][j] = Integer.parseInt(st.nextToken());
 		}
-		
+		// 회전하는 애들끼리 모아서 deque에 저장 
 		insert_Deque();		
 		
-		for (int i = 0; i < R; i++) {			
-			for (ArrayDeque<Integer> temp : list)
+		// R번 회전 시킴 deque 에 있는 마지막 원소를 빼서 첫번째에 넣음 			
+		for (ArrayDeque<Integer> temp : list) {
+			int last = R % temp.size();
+			for (int i = 0; i < last; i++)
 				temp.addFirst(temp.removeLast());
 		}
 		
+		// array에 deque에 있는 것들 다시 복귀 시킴
 		repair_array();
 		
+		// 결과 출력 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++)
 				sb.append(arr[i][j]).append(" ");
@@ -53,19 +56,23 @@ public class Main {
 		System.out.println(sb.toString());
 	}
 	
+	
+	// deque 에 있는 것들 -> array 로
 	public static void repair_array() {
 		 int direction = 0, cur_box = 0;
 		 int cur_N = N, cur_M = M;
 		 int cur_x = 0, cur_y = 0;
 		 
+		 // 사각형 개수 만큼 반복 
 		 while (cur_box < Math.min(N/2, M/2)) {
-			 
+			 // N번째 해당하는 박스 가져오기
 			 ArrayDeque<Integer> box = list.get(cur_box);
-			 
+			 // 방향 변수 
 			 direction = 0;
-			 
+			 // 박스에 들어갈 원소 개수 까지 반복 
 			 for (int count = 0; count < (cur_N*2) + Math.max(0, cur_M-2)*2; count++) {
 				 
+				 // 방향에 따른 처리 
 				 switch(direction) {
 				 case 0:
 					 if (cur_x+1 == N || arr[cur_x+1][cur_y] != 0) {
@@ -120,12 +127,13 @@ public class Main {
 		 int cur_N = N, cur_M = M;
 		 int cur_x = 0, cur_y = 0;
 		 
+		// 사각형 개수 만큼 반복 
 		 while (cur_box < Math.min(N/2, M/2)) {
-			 
+			 // 방향 변수 
 			 direction = 0;
-			 
+			 // 박스에 들어갈 원소 개수 까지 반복 
 			 for (int count = 0; count < (cur_N*2) + Math.max(0, cur_M-2)*2; count++) {
-				 
+				 // 방향에 따른 처리 
 				 switch(direction) {
 				 case 0:
 					 if (cur_x+1 == N || arr[cur_x+1][cur_y] == 0) {
