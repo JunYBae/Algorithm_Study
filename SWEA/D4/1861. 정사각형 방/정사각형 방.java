2550964c287db4d47bde382b9684ba8feed648dc
Junyeong Bae <101334646+JunYBae@users.noreply.github.com>
2025-02-27 11:51:54 +0900

    [D4] Title: 정사각형 방, Time: 410 ms, Memory: 95,580 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -1,83 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.StringTokenizer;
+
+class Point {
+	int x, y, num;
+	Point(int x, int y, int num) {
+		this.x = x;
+		this.y = y;
+		this.num = num;
+	}
+}
 
 public class Solution {
 	
-	static int N, map[][], max_move, max_room_num;
-	static int dir[][] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+	static int max_start, max_count, N, arr[][];
+	static int dir[][] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 	
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
 		
-		int T = Integer.parseInt(br.readLine());
 		for (int test_case = 1; test_case <= T; test_case++) {
 			
-			N = Integer.parseInt(br.readLine()); // 방 크기
-			map = new int[N][N]; 
-			max_move = 0; // 최대 이동 수 
-			max_room_num = Integer.MAX_VALUE; // 최대 
+			N = Integer.parseInt(br.readLine());
+			arr = new int[N][N];
+			max_count = 0;
+			max_start = Integer.MAX_VALUE;
 			
 			for (int x = 0; x < N; x++) {
 				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-				for (int y = 0; y < N; y++)
-					map[x][y] = Integer.parseInt(st.nextToken());
+				for (int y = 0; y < N; y++) 
+					arr[x][y] = Integer.parseInt(st.nextToken());
 			}
 			
 			for (int x = 0; x < N; x++) {
 				for (int y = 0; y < N; y++) {
-					int move_count = BFS(x, y);
-					
-					if (max_move < move_count) {
-						max_move = move_count;
-						max_room_num = map[x][y];
-					} else if (max_move == move_count && max_room_num > map[x][y]) {
-						max_room_num = map[x][y];
-					}
-						
-					
+					backTracking(arr[x][y], new Point(x, y, arr[x][y]), 1);
 				}
 			}
-			
-			sb.append("#" + test_case + " " + max_room_num + " " + max_move + "\n");
+
+			sb.append("#").append(test_case).append(" ").append(max_start)
+			.append(" ").append(max_count).append("\n");
 		}
 		System.out.println(sb.toString());
 	}
 	
-	public static int BFS(int start_x, int start_y) {
+	public static void backTracking(int start, Point point, int length) {
 		
-		ArrayDeque<String> queue = new ArrayDeque<>();
-		queue.add(start_x + " " + start_y);
-		int move_count = 0; // 움직인 횟수 
-		
-		while (!queue.isEmpty()) {
-			
-			int size = queue.size();
-			
-			for (int i = 0; i < size; i++) {
-				
-				String temp[] = queue.poll().split(" ");
-				int cur_x = Integer.parseInt(temp[0]);
-				int cur_y = Integer.parseInt(temp[1]);
-				
-				for (int index = 0; index < dir.length; index++) {
-					
-					int new_x = cur_x + dir[index][0];
-					int new_y = cur_y + dir[index][1];
-					
-					if (is_In(new_x, new_y) && map[cur_x][cur_y] + 1 == map[new_x][new_y])
-						queue.add(new_x + " " + new_y);
-				}
-				
-			}
-			move_count++;
+		if (max_count == length) {
+			if (max_start > start)
+				max_start = start;
 		}
+		if (max_count < length) {
+			max_count = length;
+			max_start = start;	
+		}
+			
 		
-		return move_count;
+		for (int i = 0; i < dir.length; i++) {
+			
+			int x = point.x + dir[i][0];
+			int y = point.y + dir[i][1];
+			
+			if (is_In(x, y) && point.num+1 == arr[x][y]) {
+				backTracking(start, new Point(x,y, arr[x][y]), length+1);
+			}
+		}
 	}
 	
 	public static boolean is_In(int x, int y) {
@@ -85,11 +76,5 @@ public class Solution {
 			return true;
 		return false;
 	}
+
 }
-
-
-
-
-
-
-
