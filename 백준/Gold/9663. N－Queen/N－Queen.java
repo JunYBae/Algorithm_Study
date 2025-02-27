4f823c368d975dd9391d74404638e8975db13a45
Junyeong Bae <101334646+JunYBae@users.noreply.github.com>
2025-02-27 11:01:29 +0900

    [Gold IV] Title: N-Queen, Time: 2880 ms, Memory: 13564 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,85 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
-
-
+// N*N 판에 N개의 퀸을 양립하도록 놓기
 public class Main {
 	
 	static int N, answer;
-	static int visit[][];
-	static int dir[][] = {{-1,-1}, {1, 1}, {-1, 1}, {1, -1}};
+	static boolean[] col, slash_1, slash_2; // 같은 열, 대각선(/), 대각선(\)
 	
-	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		col = new boolean[N+1];
+		slash_1 = new boolean[2*N+1];
+		slash_2 = new boolean[2*N];
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine());
-		visit = new int[N][N];
-		answer = 0;
-		
-		backTracking(0, 0);
+		setQueens(0);
 		System.out.println(answer);
 	}
 	
-	public static void backTracking(int vertex, int count) {
+	static void setQueens(int rowNo) {
 		
-		if (count == N) {
+		if (rowNo == N) {
 			answer++;
 			return;
 		}
 		
-		for (int i = 0; i < N; i++) {
-			
-			if(visit[vertex][i] == 0) {
-							
-				attack_range(vertex, i, 1);				
-				backTracking(vertex+1, count+1);
-				attack_range(vertex, i, -1);
-				
+		for (int c = 1; c <= N; c++) {
+			if (isAvailable(rowNo, c)) {
+				col[c] = slash_1[rowNo+c] = slash_2[(rowNo-c)+N] = true;
+				setQueens(rowNo+1); // 다음 퀸 놓기
+				col[c] = slash_1[rowNo+c] = slash_2[(rowNo-c)+N] = false;
 			}
 		}
-		
-		
 	}
 	
-	public static void attack_range(int x, int y, int select) {
-		
-		for (int i = 0; i < N; i++) {
-			visit[x][i] += select;
-			visit[i][y] += select;
-		}
-		
-		for (int i = 0; i < dir.length; i++) {
-			
-			int temp = 1;
-			
-			while(true) {
-				
-				int cur_x = x + dir[i][0] * temp;
-				int cur_y = y + dir[i][1] * temp;
-				
-				if (is_In(cur_x, cur_y))
-					visit[cur_x][cur_y] += select;
-				else
-					break;
-				
-				temp++;
-				
-			}
-			
-		}
-
-	}
-	
-	public static boolean is_In(int x, int y) {
-		if(x >= 0 && y >= 0 && x < N && y < N)
-			return true;
-		return false;
+	static boolean isAvailable(int rowNo, int c) {
+		return !col[c] && !slash_1[rowNo+c] && !slash_2[(rowNo-c)+N];
 	}
 }
-
