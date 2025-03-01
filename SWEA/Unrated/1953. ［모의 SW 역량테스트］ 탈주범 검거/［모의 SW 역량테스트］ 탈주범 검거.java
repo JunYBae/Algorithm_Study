839d5b3ea2eea27db241fb6766f1924f6669dd93
Junyeong Bae <101334646+JunYBae@users.noreply.github.com>
2025-03-01 15:32:04 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 109 ms, Memory: 29,440 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -7,137 +7,117 @@ import java.util.StringTokenizer;
 
 class Point {
 	int x, y;
-	
+
 	Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	
-	public String toString() {
-		return "[" + x + ", " + y + "]";
-	}
 }
 
 public class Solution {
-	
-	static int dir[][][] = {
-			{},
-			{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}, // 상,하,좌,우
-			{{-1, 0}, {1, 0}}, // 상,하
-			{{0, -1}, {0, 1}}, // 좌,우
-			{{-1, 0}, {0, 1}}, // 상,우 
-			{{1, 0}, {0, 1}},  // 하,우
-			{{1, 0}, {0, -1}}, // 하,좌
-			{{-1, 0}, {0, -1}} // 상,좌
-			};
-	
-	static int N, M, R, C, L;
-	static int answer;
+
+	static int N, M, R, C, L, answer;
 	static int map[][];
 	static boolean visit[][];
-	
+	static int dir[][][] = { 
+			{}, 
+			{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}, 
+			{{1, 0}, { -1, 0}},
+			{{0, -1}, {0, 1}}, 
+			{{-1, 0}, {0, 1}}, 
+			{{1, 0}, {0, 1}}, 
+			{{1, 0}, { 0, -1}},
+			{{-1, 0}, {0, -1}} 
+	};
+
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
 		int T = Integer.parseInt(br.readLine());
+
 		for (int test_case = 1; test_case <= T; test_case++) {
-			
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			
-			N = Integer.parseInt(st.nextToken()); // 세로 크기
-			M = Integer.parseInt(st.nextToken()); // 가로 크기
-			R = Integer.parseInt(st.nextToken()); // 맨홀 세로 위치
-			C = Integer.parseInt(st.nextToken()); // 맨홀 가로 위치
-			L = Integer.parseInt(st.nextToken()); // 탈출 후 소요된 시간
-			
-			map = new int[N][M]; // 
-			visit = new boolean[N][M]; // 방문 여부
+
+			N = Integer.parseInt(st.nextToken()); // 세로
+			M = Integer.parseInt(st.nextToken()); // 가로
+			R = Integer.parseInt(st.nextToken()); // 맨홀 - x 좌표
+			C = Integer.parseInt(st.nextToken()); // 맨홀 - y 좌표
+			L = Integer.parseInt(st.nextToken()); // 소요된 시간
+
 			answer = 0;
-			
+			map = new int[N][M];
+			visit = new boolean[N][M];
+
 			for (int x = 0; x < N; x++) {
 				st = new StringTokenizer(br.readLine(), " ");
 				for (int y = 0; y < M; y++)
 					map[x][y] = Integer.parseInt(st.nextToken());
 			}
-			
+
 			BFS(R, C);
-			sb.append("#" + test_case + " " + answer + "\n");
+			sb.append("#").append(test_case).append(" ").append(answer).append("\n");
 		}
-		
 		System.out.println(sb.toString());
 	}
-	
+
 	public static void BFS(int start_x, int start_y) {
-		
-		ArrayDeque<Point> queue = new ArrayDeque<>();
+
+		ArrayDeque<Point> queue = new ArrayDeque<Point>();
 		queue.add(new Point(start_x, start_y));
-		
-		int cur_time = 0; // 현재 시간
-		int cur_count = 0; // 현재 파이프 개수 
-		visit[start_x][start_y] = true; // 맨홀 위치 방문 true
-		
-		// 큐가 비었거나, 시간 다 되면 
-		while(!queue.isEmpty() && !(cur_time == L)) {
-			
-//			System.out.println(queue.toString());
-			
+		visit[start_x][start_y] = true;
+
+		int cur_day = 0;
+
+		while (!queue.isEmpty() && cur_day != L) {
+
 			int size = queue.size();
-			cur_count += size;
-			
 			for (int i = 0; i < size; i++) {
-				
+
 				Point point = queue.poll();
-				
-				int cur_pipe = map[point.x][point.y]; // 무슨 파이프인지
-				int pipe_list[][] = dir[cur_pipe]; // 해당하는 파이프 리스트 
-				
-				for (int index = 0; index < pipe_list.length; index++) {
-					
-					int cur_x = point.x + pipe_list[index][0];
-					int cur_y = point.y + pipe_list[index][1];
-					
-					if (is_In(cur_x, cur_y) && is_connect(point.x, point.y, cur_x, cur_y)) {
-						visit[cur_x][cur_y] = true;
-						queue.add(new Point(cur_x, cur_y));
+				int struct = map[point.x][point.y];
+
+				for (int index = 0; index < dir[struct].length; index++) {
+
+					int x = point.x + dir[struct][index][0];
+					int y = point.y + dir[struct][index][1];
+
+					if (is_In(x, y) && map[x][y] != 0 && !visit[x][y]) {
+
+						if (is_possible(point, new Point(x, y))) {
+							visit[x][y] = true;
+							queue.add(new Point(x, y));
+						}
 					}
-					
 				}
-				
+
+				answer++;
 			}
-			
-			cur_time++;
+
+			cur_day++;
 		}
-		
-		answer = cur_count;
+
 	}
-	
+
+	public static boolean is_possible(Point start, Point end) {
+
+		int struct = map[end.x][end.y];
+
+		for (int index = 0; index < dir[struct].length; index++) {
+			
+			int x = end.x + dir[struct][index][0];
+			int y = end.y + dir[struct][index][1];
+			
+			if(x == start.x && y == start.y)
+				return true;
+		}
+
+		return false;
+	}
+
 	public static boolean is_In(int x, int y) {
-		if (x >= 0 && y >= 0 && x < N && y < M && !visit[x][y] && map[x][y] != 0) 
+		if (x >= 0 && y >= 0 && x < N && y < M)
 			return true;
 		return false;
 	}
-	
-	public static boolean is_connect(int src_x, int src_y, int dst_x, int dst_y) {
-		
-		int dst_pipe_list[][] = dir[map[dst_x][dst_y]];
-		ArrayList<String> dst_point = new ArrayList<>();
-		
-		for (int i = 0; i < dst_pipe_list.length; i++) {
-			int x = dst_x + dst_pipe_list[i][0];
-			int y = dst_y + dst_pipe_list[i][1];
-			dst_point.add(x + " " + y);
-		}
-		
-		for (String point : dst_point) {
-			if (dst_point.contains(src_x + " " + src_y))
-				return true;
-		}
-			
-			return false;
-	}
 }
-
-
-
