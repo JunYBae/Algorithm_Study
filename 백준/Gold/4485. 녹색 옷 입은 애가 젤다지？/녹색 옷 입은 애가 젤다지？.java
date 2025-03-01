0b14a58c3cb46a02d869ff7a987b704c04fd3b90
Junyeong Bae <101334646+JunYBae@users.noreply.github.com>
2025-03-01 19:39:54 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 152 ms, Memory: 17896 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,79 +1,88 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 class Point implements Comparable<Point>{
 	int x, y, cost;
+
 	public Point(int x, int y, int cost) {
+		super();
 		this.x = x;
 		this.y = y;
 		this.cost = cost;
 	}
+
 	@Override
 	public int compareTo(Point o) {
 		return Integer.compare(this.cost, o.cost);
 	}
+	
 }
+
 
 public class Main {
 	
-	static int N, map[][], answer[][];
-	static int dir[][] = {{-1,0}, {1,0}, {0,-1}, {0,1}};
-	
+	static int T, map[][], cost[][];
+	static int dir[][] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 	
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int count = 1;
-		
-		while((N = Integer.parseInt(br.readLine())) != 0) {
+		StringBuilder sb = new StringBuilder();
+		int test_case = 1;
+
+		while((T = Integer.parseInt(br.readLine()))!= 0) {
 			
-			map = new int[N][N];
-			answer = new int[N][N];
-			
-			for (int i = 0; i < N; i++) {
+			map = new int[T][T];
+			cost = new int[T][T];
+			for (int x = 0; x < T; x++) {
 				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-					answer[i][j] = Integer.MAX_VALUE;
-				}
+				Arrays.fill(cost[x], Integer.MAX_VALUE);
+				for (int y = 0; y < T; y++)
+					map[x][y] = Integer.parseInt(st.nextToken());
 			}
+		
+			cost[0][0] = map[0][0];
+			int answer = BFS(new Point(0, 0, cost[0][0]));
 			
-			answer[0][0] = map[0][0];
-			best_route();
-			
-			System.out.println("Problem " + (count++) + ": " + answer[N-1][N-1]);
+			sb.append("Problem ").append(test_case).append(": ").append(answer).append("\n");
+			test_case++;
 		}
+		System.out.println(sb.toString());
 	}
 	
-	public static void best_route() {
-		PriorityQueue<Point> queue = new PriorityQueue<>();
-		queue.add(new Point(0, 0, map[0][0]));
+	public static int BFS(Point start) {
+		
+		PriorityQueue<Point> queue = new PriorityQueue<Point>();
+		queue.add(start);
 		
 		while(!queue.isEmpty()) {
 			
 			Point point = queue.poll();
 			
-			if (map[point.x][point.y] == -1)
-				continue;
+			if (point.x == T-1 && point.y == T-1)
+				return point.cost;
 			
-			for (int i = 0; i < 4; i++) {
-				int x = point.x + dir[i][0];
-				int y = point.y + dir[i][1];
+			for (int index = 0; index < 4; index++) {
+				int x = point.x + dir[index][0];
+				int y = point.y + dir[index][1];
 				
-				if (x >= 0 && y >= 0 && x < N && y < N && map[x][y] != -1) {
-					int new_route = point.cost + map[x][y];
-					if (new_route < answer[x][y]) {
-						queue.add(new Point(x, y, new_route));
-						answer[x][y] = new_route;
-					}
+				if (x >= 0 && y >= 0 && x < T && y < T && cost[x][y] > point.cost + map[x][y]) {
+					cost[x][y] = point.cost + map[x][y];
+					queue.add(new Point(x, y, cost[x][y]));
 				}
-				
-			}
-			
+			}			
 		}
+		
+		return -1;
 	}
 }
+
+
 
