47c35dc186ef1b2fa0197adad1cb075b23620944
Junyeong Bae <101334646+JunYBae@users.noreply.github.com>
2025-03-01 18:36:15 +0900

    [Unrated] Title: [모의 SW 역량테스트] 활주로 건설, Time: 101 ms, Memory: 27,136 KB -BaekjoonHub

--- SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
@@ -1,101 +1,91 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
 	
-	static int N, X; // 활주로 크기, 경사로 길이 
-	static int road_count; // 지을 수 있는 활주로 개수 
-	static int map[][]; // 현재 활주로 정보 저장 
-	
+	static int N, X, answer, map[][];
+
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder(); // 저장 
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		
+
 		for (int test_case = 1; test_case <= T; test_case++) {
 			
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken()); // 활주로 크기
+			
+			N = Integer.parseInt(st.nextToken()); // 지형 맵 크기
 			X = Integer.parseInt(st.nextToken()); // 경사로 길이
-			map = new int[N][N]; // 활주로 맵
-			road_count = 0; // 활주로 개수 초기화
+			map = new int[N][N]; // 맵
+			answer = 0;
 			
 			for (int x = 0; x < N; x++) {
 				st = new StringTokenizer(br.readLine(), " ");
-				for (int y = 0; y < N; y++) 
-					map[x][y] = Integer.parseInt(st.nextToken()); // 맵 정보 저장
+				for (int y = 0; y < N; y++)
+					map[x][y] = Integer.parseInt(st.nextToken());
 			}
 			
-			for (int x = 0; x < N; x++) // 활주로 건설 (가로) 
-				build_road(map[x].clone());
-			
-			for (int y = 0; y < N; y++) {
+			for (int x = 0; x < N; x++) {
+				if(simulation(map[x]))
+					answer++;
+				
 				int temp[] = new int[N];
-				for (int x = 0; x < N; x++) {
-					temp[x] = map[x][y];
-				}
-				build_road(temp);
+				
+				for (int y = 0; y < N; y++)
+					temp[y] = map[y][x];
+				
+				if(simulation(temp))
+					answer++;
 			}
-			
-			
-//			for (int y = 0; y < N; y++) {
-//				build_road(0, y, false); // 활주로 건설 (세로)
-//				}
-			
-			sb.append("#" + test_case + " " + road_count + "\n");
+					
+			sb.append("#").append(test_case).append(" ").append(answer).append("\n");
 		}
-		
 		System.out.println(sb.toString());
 	}
 	
-	public static void build_road(int road[]) {
+	public static boolean simulation(int arr[]) {
 		
-		int prev_node = road[0];
-		int dong_count = 0;
+		int prev = arr[0];
+		int count = 0;
 		
-		for (int i = 0; i < road.length; i++) {
+		for (int index = 0; index < arr.length; index++) {
 			
-			if (prev_node == road[i]) { // 같으면 dong_count 추가
-				dong_count++;
-			} else if (Math.abs(prev_node - road[i]) > 1) { // 차이가 1 초과면 못해
-				return;
-			} else if (prev_node < road[i]) { // 높으면? dong_count 이용! 
-				if (dong_count >= X) // 더 많으면? 통과임! 
-					dong_count = 1;
-				else // 못함
-					return; 
-			} else if (prev_node > road[i]) { // 낮으면? 전진 탐색 시작 
-				if (is_possible(i, road)) {
-					dong_count = 0;
-					i += X-1;
-				}
-				else // 못함
-					return; 
+			if (arr[index] == prev) {
+				count++;
 			}
 			
-			prev_node = road[i];
-		}
-		
-//		System.out.println("possible : " + Arrays.toString(road));
-		
-		road_count++;
-	}
-	
-	public static boolean is_possible(int index, int road[]) {
+			else if (prev < arr[index] && prev + 1 == arr[index]) {
+				if (count < X) 
+					return false;
+				count = 1;
+			}
+			
+			else if (prev > arr[index] && prev - 1 == arr[index]) {
+				int w = 0;
+				for (int t = index; t < Math.min(N, index+X); t++) {
+					if (arr[index] != arr[t])
+						return false;
+					else
+						w++;
+				}
 				
-		int select = road[index]; // 목표노드 (이게 반복되야함)
-		int count = 0; // 목표 노드 개수 
-		for (int i = index; i < index+X && i < N; i++) {
-			if (road[i] == select)
-				count++;
+				if (w < X)
+					return false;
+				
+				count = 0;
+				index += X - 1;
+			}
+			
+			else 
+				return false;
+	
+			prev = arr[index];
 		}
-		if (count >= X) {
-			return true;
-		}
-		else
-			return false;
+		
+		return true;
 	}
 }
+
