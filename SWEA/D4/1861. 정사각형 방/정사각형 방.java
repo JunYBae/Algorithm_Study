7be7a6611be95586f6c87ee9dd9447b273833be1
Junyeong Bae <101334646+JunYBae@users.noreply.github.com>
2025-03-01 19:52:38 +0900

    [D4] Title: 정사각형 방, Time: 766 ms, Memory: 115,968 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -1,80 +1,105 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 class Point {
-	int x, y, num;
-	Point(int x, int y, int num) {
+	int x, y, count;
+
+	public Point(int x, int y, int count) {
 		this.x = x;
 		this.y = y;
-		this.num = num;
-	}
+		this.count = count;
+	}	
 }
+
 
 public class Solution {
 	
-	static int max_start, max_count, N, arr[][];
+	static int N, map[][];
+	static boolean visit[][];
+	static int max, max_num;
 	static int dir[][] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 	
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
 		
+		int T = Integer.parseInt(br.readLine());
 		for (int test_case = 1; test_case <= T; test_case++) {
 			
 			N = Integer.parseInt(br.readLine());
-			arr = new int[N][N];
-			max_count = 0;
-			max_start = Integer.MAX_VALUE;
+			map = new int[N][N];
+			max = 0; max_num = 0;
 			
 			for (int x = 0; x < N; x++) {
 				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-				for (int y = 0; y < N; y++) 
-					arr[x][y] = Integer.parseInt(st.nextToken());
+				for (int y = 0; y < N; y++)
+					map[x][y] = Integer.parseInt(st.nextToken());
 			}
 			
 			for (int x = 0; x < N; x++) {
 				for (int y = 0; y < N; y++) {
-					backTracking(arr[x][y], new Point(x, y, arr[x][y]), 1);
+					visit = new boolean[N][N];
+					visit[x][y] = true;
+					DFS(x, y, map[x][y], map[x][y], 1);
 				}
 			}
-
-			sb.append("#").append(test_case).append(" ").append(max_start)
-			.append(" ").append(max_count).append("\n");
+			
+			sb.append("#").append(test_case).append(" ").append(max_num).append(" ").append(max).append("\n");
 		}
 		System.out.println(sb.toString());
 	}
 	
-	public static void backTracking(int start, Point point, int length) {
+	public static void DFS(int x, int y, int start, int cur_num, int count) {
 		
-		if (max_count == length) {
-			if (max_start > start)
-				max_start = start;
+		if (max < count) {
+			max = count;
+			max_num = start;
+		}	
+		else if (max == count) {
+			if (start < max_num)
+				max_num = start;
 		}
-		if (max_count < length) {
-			max_count = length;
-			max_start = start;	
-		}
 			
-		
-		for (int i = 0; i < dir.length; i++) {
+		for (int index = 0; index < dir.length; index++) {
 			
-			int x = point.x + dir[i][0];
-			int y = point.y + dir[i][1];
+			int cur_x = x + dir[index][0];
+			int cur_y = y + dir[index][1];
 			
-			if (is_In(x, y) && point.num+1 == arr[x][y]) {
-				backTracking(start, new Point(x,y, arr[x][y]), length+1);
+			if (is_In(cur_x, cur_y) && cur_num + 1 == map[cur_x][cur_y]) {
+				visit[cur_x][cur_y] = true; 
+				DFS(cur_x, cur_y, start, map[cur_x][cur_y], count+1);
+				visit[cur_x][cur_y] = false;
 			}
 		}
 	}
 	
 	public static boolean is_In(int x, int y) {
-		if (x >= 0 && y >= 0 && x < N && y < N)
+		if (x >= 0 && y >= 0 && x < N && y < N && !visit[x][y])
 			return true;
 		return false;
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
