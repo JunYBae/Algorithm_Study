feb7d60a4a82095dd309f21516ab94a92620485a
Junyeong Bae <101334646+JunYBae@users.noreply.github.com>
2025-03-01 19:23:36 +0900

    [Unrated] Title: [모의 SW 역량테스트] 무선 충전, Time: 119 ms, Memory: 30,208 KB -BaekjoonHub

--- SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
+++ SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
@@ -2,19 +2,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class BC implements Comparable<BC>{
-	int x, y, range, power;
-	BC(int x, int y, int range, int power) {
+class Point {
+	int x, y;
+
+	public Point(int x, int y) {
+		super();
 		this.x = x;
 		this.y = y;
+	}
+	
+}
+
+class Station extends Point implements Comparable<Station> {
+	int range, power;
+
+	public Station(int x, int y, int range, int power) {
+		super(x, y);
 		this.range = range;
 		this.power = power;
 	}
+
 	@Override
-	public int compareTo(BC o) {
+	public int compareTo(Station o) {
 		return Integer.compare(this.power, o.power) * -1;
 	}
 	
@@ -25,131 +38,120 @@ class BC implements Comparable<BC>{
 	
 	@Override
 	public int hashCode() {
-		return (x +  " " + y).hashCode();
+		return (x + " " + y).hashCode();
 	}
 	
 }
 
 public class Solution {
 	
-	static int M, A;
-	static int persons_move[][]; // 사용자들 움직임 저장
-	static BC bc_tower[]; // 충전소 정보 저장
-	// 이동 X, 상, 우, 하, 좌
-	static int dir[][] = {{0, 0}, {0, -1}, {1, 0}, {0, 1}, {-1, 0}}; 
+	static int M, A, answer;
+	static int move[][];
+	static ArrayList<Station> station_list;
+	// X, 상, 우, 하, 좌
+	static int dir[][] = {{0, 0}, {0, -1}, {1, 0}, {0, 1}, {-1, 0}};
 	
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		
+
 		for (int test_case = 1; test_case <= T; test_case++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
 			
-			M = Integer.parseInt(st.nextToken()); // 총 이동 시간 
-			A = Integer.parseInt(st.nextToken()); // BC(충전소) 개수
-			
-			persons_move = new int[M+1][2]; // 사용자들 움직임 저장 
-			persons_move[0][0] = 0; // 초기
-			persons_move[0][1] = 0; // 초기
-			
-			for (int x = 0; x < 2; x++) {
-				st = new StringTokenizer(br.readLine());
-				for (int y = 1; y <= M; y++) 
-					persons_move[y][x] = Integer.parseInt(st.nextToken());
-			}
-			
-			bc_tower = new BC[A]; // 충전소 정보 저장
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			M = Integer.parseInt(st.nextToken()); // 움직인 횟수 
+			A = Integer.parseInt(st.nextToken()); // 스테이션 개수
+			answer = 0;
+			station_list = new ArrayList<Station>();
+			move = new int[M+1][2];
+			move[0][1] = 0; move[0][0] = 0; // 처음엔 안움직임
+					
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int i = 1; i <= M; i++) // A 플레이어
+				move[i][0] = Integer.parseInt(st.nextToken());
+					
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int i = 1; i <= M; i++) // B 플레이어 
+				move[i][1] = Integer.parseInt(st.nextToken());
 			
 			for (int i = 0; i < A; i++) {
-				st = new StringTokenizer(br.readLine());
-				
+				st = new StringTokenizer(br.readLine(), " ");
 				int x = Integer.parseInt(st.nextToken());
 				int y = Integer.parseInt(st.nextToken());
 				int range = Integer.parseInt(st.nextToken());
 				int power = Integer.parseInt(st.nextToken());
-				
-				bc_tower[i] = new BC(x, y, range, power);
+				station_list.add(new Station(x, y, range, power));
 			}
 			
-
-			int total_charge = simulation_Move();
-			System.out.println("#" + test_case + " " + total_charge);
+			simulation();
 			
-		}	
+			sb.append("#").append(test_case).append(" ").append(answer).append("\n");
+		}
+		System.out.println(sb.toString());
 	}
 	
-	public static int simulation_Move() {
+	public static void simulation() {
 		
-		int total_charge = 0; // 총 충전량
-		int per1_x = 1, per1_y = 1, per2_x = 10, per2_y = 10; // 사람 좌표
+		Point player_a = new Point(1, 1);
+		Point player_b = new Point(10, 10);
 		
 		for (int i = 0; i <= M; i++) {
-			// 좌표 움직임 
-			per1_x += dir[persons_move[i][0]][0];
-			per1_y += dir[persons_move[i][0]][1];
-			per2_x += dir[persons_move[i][1]][0];
-			per2_y += dir[persons_move[i][1]][1];
+			player_a.x += dir[move[i][0]][0];
+			player_a.y += dir[move[i][0]][1];
+			player_b.x += dir[move[i][1]][0];
+			player_b.y += dir[move[i][1]][1];
 			
-//			System.out.println("person 1 : " + per1_x + " " + per1_y);
-//			System.out.println("person 2 : " + per2_x + " " + per2_y);
-//			System.out.println();
+			PriorityQueue<Station> station_a = check_station(player_a);
+			PriorityQueue<Station> station_b = check_station(player_b);
 			
-			PriorityQueue<BC> per1_towers = calcul_BC(per1_x, per1_y); // 사용자 1의 주변 BC 충전소 
-			PriorityQueue<BC> per2_towers = calcul_BC(per2_x, per2_y); // 사용자 2의 주변 BC 충전소
-			
-			if (per1_towers.isEmpty() && per2_towers.isEmpty())
+			if (station_a.size() == 0 && station_b.size() == 0)
 				continue;
 			
-			total_charge += select_tower(new ArrayList<BC>(per1_towers), new ArrayList<BC>(per2_towers));
+			answer += calculate(new ArrayList<Station>(station_a), new ArrayList<Station>(station_b));
 		}
-		
-		return total_charge;
 	}
 	
+	public static int calculate(ArrayList<Station> station_a, ArrayList<Station> station_b) {
+		
+		int max = 0;
+		
+		if (station_a.size() == 0)
+			return station_b.get(0).power;
+		
+		else if (station_b.size() == 0)
+			return station_a.get(0).power;
+		
+		for (Station a : station_a) {
+			for (Station b : station_b) {				
+				int sum = 0;
+				
+				if(a.equals(b))
+					sum = a.power;			
+				else
+					sum = a.power + b.power;
+				
+				if (max < sum)
+					max = sum;
+			}
+		}	
+		
+		return max;
+	}
 	
-	public static PriorityQueue<BC> calcul_BC(int x, int y) {
+	public static PriorityQueue<Station> check_station(Point player) {
 		
-		PriorityQueue<BC> list = new PriorityQueue<>();
+		PriorityQueue<Station> list = new PriorityQueue<Station>();
 		
-		for (BC tower : bc_tower) {	
-			boolean touch = ((Math.abs(tower.x-x) + Math.abs(tower.y-y)) <= tower.range);
-			if (touch)
-				list.add(tower);
+		for (Station station : station_list) {
+			int distance = Math.abs(player.x - station.x) + Math.abs(player.y - station.y);
+			if(station.range >= distance)
+				list.add(station);
 		}
 		
 		return list;
 	}
-	
-	public static int select_tower(ArrayList<BC> per1, ArrayList<BC> per2) {
-		
-		int max_charge = 0;
-		
-		if(per1.isEmpty())
-			return per2.get(0).power;
-		else if (per2.isEmpty())
-			return per1.get(0).power;
-		
-		for (int i = 0; i < per1.size(); i++) {			
-			for (int j = 0; j < per2.size(); j++) {
-				
-				int temp_sum = 0;
-				
-				BC test1 = per1.get(i);
-				BC test2 = per2.get(j);
-				
-				if (test1.equals(test2)) 
-					temp_sum = test1.power;
-				
-				else
-					temp_sum += test1.power + test2.power;
-				
-				if (temp_sum > max_charge)
-					max_charge = temp_sum;
-				
-			}
-		}
-		
-		return max_charge;
-	}
 }
+
+
+
