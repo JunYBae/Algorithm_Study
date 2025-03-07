f231c311b53ddfcb04c47f58baba468f37c7ac1a
Junyeong Bae <101334646+JunYBae@users.noreply.github.com>
2025-03-07 11:50:52 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 112 ms, Memory: 29,440 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -3,91 +3,77 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 class Node {
-	int value; // 이건 현재 노드의 값임 
-	ArrayList<Integer> edge = new ArrayList<Integer>(); // 현재 연결되어 있는 노드 값
-	
-	Node(int value) {
-		this.value = value;
-	}
-	
-	Node(int value, int edge) {
-		this.value = value;
-		this.edge.add(edge);
-	}
-	
-	public int edge_size() {
-		return edge.size();
+	int vertex;
+	HashSet<Integer> edge_list = new HashSet<>();
+	Node(int vertex) {
+		this.vertex = vertex;
 	}
 }
 
 public class Solution {
 	
-	static int V, E;
-	static Node map[];
-	static int node_connect_size[]; // 각 노드들이 연결되어 있는 개수 ?
-	static ArrayDeque<Node> queue; // 노드들 넣는 큐 (참조없는 노드들)
+	static int V, E, map[];
+	static Node node_list[];
 	
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
+		//int T = Integer.parseInt(br.readLine());
 		for (int test_case = 1; test_case <= 10; test_case++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			
-			V = Integer.parseInt(st.nextToken()); // 정점의 개수 
-			E = Integer.parseInt(st.nextToken()); // 간선의 개수
-			map = new Node[V+1]; // 이건 현재 인덱스의 노드를 저장 
-			node_connect_size = new int[V+1]; // 각 노드들이 연결되어 있는 개수 ?
-			queue = new ArrayDeque<>(); // 노드들을 저장 
+			sb.append("#").append(test_case).append(" ");
 			
-			for (int i = 1; i <= V; i++)
-				map[i] = new Node(i); // 초기화
-			
-			st = new StringTokenizer(br.readLine(), " "); 
-			for (int i = 0; i < E; i++) {
-				int start = Integer.parseInt(st.nextToken()); // 시작노드
-				int end = Integer.parseInt(st.nextToken()); // 도착 노드
-				
-				node_connect_size[end]++; // 도착노드의 참조 노드 개수 추가				
-				map[start].edge.add(end);
-			}
+			V = Integer.parseInt(st.nextToken());
+			E = Integer.parseInt(st.nextToken());
+			map = new int[V+1];
+			node_list = new Node[V+1];
 			
 			for (int i = 1; i <= V; i++) {
-				if (node_connect_size[i] == 0) // 만약 루트(루트는 아니지만) 노드이면?
-					queue.add(map[i]); // 넣어주기 
+				node_list[i] = new Node(i);
 			}
 			
-			StringBuilder sb = new StringBuilder(); 
-			sb.append("#" + test_case +" "); // 처음 출력 초기화
-			progress_task(sb); // 일처리 시작 
-			 
-			System.out.println(sb.toString()); // 결과 출력
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int i = 1; i <= E; i++) {
+				int start = Integer.parseInt(st.nextToken());
+				int end = Integer.parseInt(st.nextToken());
+				
+				map[end]++;
+				node_list[start].edge_list.add(end);
+			}
+			
+			ArrayDeque<Node> queue = new ArrayDeque<>();
+			for (int i = 1; i <= V; i++) {
+				if (map[i] == 0)
+					queue.add(node_list[i]);
+			}
+			
+			BFS(queue, sb);
+			sb.append("\n");
 		}
+		System.out.println(sb.toString());
 	}
 	
-	
-	public static void progress_task(StringBuilder sb) {
+	public static void BFS(ArrayDeque<Node> queue, StringBuilder sb) {
 		
-		while(!queue.isEmpty()) { // 큐가 빌때 까지 진행
+		while(!queue.isEmpty()) {
 			
-			Node cur_node = queue.poll(); // 현재 마지막 노드 뺌 
-			sb.append(cur_node.value + " "); // 현재 노드 값 넣어줌
+			Node node = queue.poll();
+			sb.append(node.vertex).append(" ");
 			
-			ArrayList<Integer> cur_edge = cur_node.edge; // 현재 노드와 연결되어있는 노드들
-			
-			while(!cur_edge.isEmpty()) { // 연결되어 있는 노드들 찾아서 edge 제거
-				int temp_edge = cur_edge.remove(0);
-				node_connect_size[temp_edge]--;
+			for (int next_node : node.edge_list) {
+				map[next_node]--;
 				
-				if(node_connect_size[temp_edge] == 0) { // 만약 나를 참조하는 노드가 없으면?
-					queue.add(map[temp_edge]); // 노드 넣어주기
-				}
+				if (map[next_node] == 0)
+					queue.add(node_list[next_node]);
 			}
 		}
-
 	}
 }
+
+
